ebd7c683f04217481e0cc8b87aa9a67d0364b872
ivyxuan <iloveivyxuan@gmail.com>
2020-08-20 22:48:53 -0500

    Add basic command center

--- tortoise-editor.js
+++ tortoise-editor.js
@@ -1,9 +1,9 @@
 // Editor: Handle the interaction of CodeMirror main editor.
 // REFACTOR NEEDED IF WE ARE GOING TO SUPPORT MORE STATUSES
 Editor = function() {
-    var Editor = {};
+	var Editor = {};
 	var MainEditor = null;
-	
+
 	// UI support
 	// Obseleted: Tips
 	// Show the tips
@@ -19,7 +19,7 @@ Editor = function() {
 		TipsActive = false;
 		Editor.ClearHighlights();
 	}
-	
+
 	// ShowErrors: Show the error tips & markers.
 	Editor.ShowErrors = function(Error) {
 		Editor.ClearHighlights();
@@ -37,7 +37,7 @@ Editor = function() {
 			Highlights[I].Clear();
 		Highlights = [];
 	}
-	
+
 	// Class: Highlight
 	var Highlight = function(Type, Source) {
 		this.Type = Type;
@@ -46,15 +46,15 @@ Editor = function() {
 		var Accumulated = 0;
 		for (var N = 0; N < LineCount; N++) {
 			var Length = MainEditor.getLine(N).length;
-			if (this.PositionFrom == null && Source.start <= Accumulated + Length) this.PositionFrom = { line: N, ch: Source.start - Accumulated };	
-			if (this.PositionTo == null && Source.end <= Accumulated + Length) this.PositionTo = { line: N, ch: Source.end - Accumulated };				
+			if (this.PositionFrom == null && Source.start <= Accumulated + Length) this.PositionFrom = { line: N, ch: Source.start - Accumulated };
+			if (this.PositionTo == null && Source.end <= Accumulated + Length) this.PositionTo = { line: N, ch: Source.end - Accumulated };
 			if (this.PositionFrom != null && this.PositionTo != null) break;
 			Accumulated += Length + 1;
 		}
 	}
 	Highlight.prototype.Clear = function() {
 		if (this.TextMarker != null) this.TextMarker.clear();
-		if (this.Gutter != null) MainEditor.doc.setGutterMarker(this.PositionFrom.line, this.Type, null); 
+		if (this.Gutter != null) MainEditor.doc.setGutterMarker(this.PositionFrom.line, this.Type, null);
 		if (this.TipsWidget != null) this.HideTips();
 	}
 	Highlight.prototype.ScrollIntoView = function(Distance = 200) {
@@ -73,16 +73,16 @@ Editor = function() {
 	}
 	Highlight.prototype.HideTips = function() {
 		if (this.TipsWidget == null) return;
-		this.TipsWidget.clear(); 
+		this.TipsWidget.clear();
 		this.TipsWidget = null;
 	}
 	Highlight.prototype.ShowGutter = function() {
 		this.Gutter = $("<div class='CodeMirror-marker-" + this.Type + "'></div>")[0];
 		this.Gutter.Callback = () => this.ShowTips();
-		MainEditor.doc.setGutterMarker(this.PositionFrom.line, this.Type, this.Gutter); 
+		MainEditor.doc.setGutterMarker(this.PositionFrom.line, this.Type, this.Gutter);
 		return this;
 	}
-	
+
 	// Editor support
 	// SetContent: Set the content of the editor.
 	var Generation;
@@ -99,34 +99,34 @@ Editor = function() {
 		// Event listener
 		MainEditor.on("changes", () => Editor.Call("###Unapplied"));
 	}
-	
+
 	// GetContent: Get the content of the editor.
 	Editor.GetContent = function() {
 		return MainEditor.getValue();
 	}
-	
+
 	// SetApplied: Set applied status.
 	Editor.SetApplied = function() {
 		Generation = MainEditor.doc.changeGeneration();
 	}
-	
+
 	// SetReadonly: Set readonly status.
 	Editor.SetReadonly = function(Status) {
 		MainEditor.setOption("readOnly", Status);
 	}
-	
+
 	// Undo: Undo last change.
 	Editor.Undo = function() {
 		if (MainEditor.getOption("readOnly")) return;
 		MainEditor.doc.undo();
 	}
-	
+
 	// Redo: Redo last change.
 	Editor.Redo = function() {
 		if (MainEditor.getOption("readOnly")) return;
 		MainEditor.doc.redo();
 	}
-	
+
 	// Initialize the editor.
 	Editor.Initialize = function() {
 		// Basic initialization
@@ -145,11 +145,11 @@ Editor = function() {
 			var token = cm.getTokenAt(cur);
 			var to = CodeMirror.Pos(cur.line, token.end);
 			if (token.string && /\S/.test(token.string[token.string.length - 1])) {
-			  term = token.string
-			  from = CodeMirror.Pos(cur.line, token.start)
+				term = token.string
+				from = CodeMirror.Pos(cur.line, token.start)
 			} else {
-			  term = ''
-			  from = to
+				term = ''
+				from = to
 			}
 			found = options.words.filter((word) => word.slice(0, term.length) == term)
 			if (found.length > 0)
@@ -176,66 +176,95 @@ Editor = function() {
 		Overlays.Initialize();
 		Editor.MainEditor = MainEditor;
 	}
-	
+
 	// Engine features
 	// Resize: Resize the viewport width (on mobile platforms)
 	var Resize = function(Width) {
 		$("#viewport").attr("content", "width=" + Width + ",user-scalable=no,viewport-fit=cover");
 	}
 	var ResizeHandler = null;
-    Editor.Resize = function (Width) {
-        if (navigator.userAgent.indexOf("NetLogo") != -1) {
-            if (ResizeHandler != null) clearTimeout(ResizeHandler);
-            // On some iOS devices, the animated rotation takes some time
-            // and sometimes it causes the scaling to be nullified...
-            ResizeHandler = setTimeout(function () {
-                Resize(Width - 1);
-                ResizeHandler = setTimeout(function () {
-                    Resize(Width);
-                    ResizeHandler = null;
-                }, 100);
-            }, 100);
-        } else Resize(Width);
+	Editor.Resize = function (Width) {
+		if (navigator.userAgent.indexOf("NetLogo") != -1) {
+			if (ResizeHandler != null) clearTimeout(ResizeHandler);
+			// On some iOS devices, the animated rotation takes some time
+			// and sometimes it causes the scaling to be nullified...
+			ResizeHandler = setTimeout(function () {
+					Resize(Width - 1);
+					ResizeHandler = setTimeout(function () {
+							Resize(Width);
+							ResizeHandler = null;
+					}, 100);
+			}, 100);
+		} else Resize(Width);
 	}
-	
-	// Call: Call the Unity engine. 
+
+	// Call: Call the Unity engine.
 	Editor.Call = function(Code) {
 		PostMessage(Code);
 	}
-	
+
 	return Editor;
 }();
 
 // Overlays: Overlays manager.
 Overlays = function() {
 	var Overlays = {};
-	
+
 	// Initialize: Initialize all overlays.
 	Overlays.Initialize = function() {
 		// RotateScreen: Rotate-Screen dialog.
 		Overlays.RotateScreen = $("#Rotate-Screen").asOverlay().click(() => Overlays.RotateScreen.Hide());
 	}
-	
+
 	return Overlays;
 }();
 
 // Localized: Localized support.
 Localized = function() {
 	var Localized = {};
-	
+
 	// Initialize: Initialize the manager with given data.
 	Localized.Initialize = function(Data) {
 		Localized.Data = Data;
 		$(".Localized").each((Index, Target) => $(Target).text(Localized.Get($(Target).text())));
 	}
-	
+
 	// Get: Get localized string.
 	Localized.Get = function(Source) {
 		if (Localized.Data.hasOwnProperty(Source)) return Localized.Data[Source];
 		return Source;
 	}
-	
+
 	return Localized;
 }();
 
 // Commands: Handle the interaction of CodeMirror command center.
+Commands = function() {
+	var Commands = {};
+
+	Commands.Show = function() {
+		$('#Main-Editor').css("display", "none");
+	}
+
+	Commands.Hide = function() {
+		$('#Main-Editor').css("display", "block");
+	}
+
+	Commands.PrintInput = function(Objective, Content) {
+		$('.command-output').append(`
+			<p class="Localized comment">${Objective}> ${Content}</p>
+		`)
+	}
+
+	Commands.PrintOutput = function(Content) {
+		$('.command-output').append(`
+			<p class="Localized comment">${Content}</p>
+		`)
+	}
+
+	Commands.Compile = function(Content) {
+		return Content
+	}
+
+	return Commands;
+}();
